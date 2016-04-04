5d93f9e346057f180ea9f4d9f102a666c1bff030
Carmen Brando <cvbrandoe@users.noreply.github.com>
2016-04-04 13:06:15 +0200

    initial upload Reden online web app

---
+++ data/places-in-tei.js
@@ -0,0 +1,12 @@
+var geoData = {"type":"FeatureCollection","features":[
+{"type":"Feature","properties":{"dbpediaUri":"http://dbpedia.org/page/Rome/", "geoNamesUri":"http://sws.geonames.org/3169070/","level":"2","name":"Rome","occurrences":11},"geometry":{"type":"Point","coordinates":[12.51133,41.89193]}},
+
+{"type":"Feature","properties":{"dbpediaUri":"http://dbpedia.org/page/Amsterdam/", "geoNamesUri":"http://sws.geonames.org/2993458/","level":"2","name":"Amsterdam","occurrences":61},"geometry":{"type":"Point","coordinates":[4.88969,52.37403]}},
+
+{"type":"Feature","properties":{"dbpediaUri":"http://en.wikipedia.org/wiki/Upper_Normandy", "geoNamesUri":"http://sws.geonames.org/3013756/","level":"4","name":"Haute-Normandie","occurrences":110},"geometry":{"type":"Point","coordinates":[1,49.41667]}},
+
+{"type":"Feature","properties":{"dbpediaUri":"http://en.wikipedia.org/wiki/Lower_Normandy", "geoNamesUri":"http://sws.geonames.org/3034693/","level":"4","name":"Basse-Normandie","occurrences":18},"geometry":{"type":"Point","coordinates":[-0.53333, 48.93333]}},
+
+{"type":"Feature","properties":{"dbpediaUri":"http://en.wikipedia.org/wiki/Lower_Normandy", "geoNamesUri":"http://sws.geonames.org/3034693/","level":"4","name":"Picardie","occurrences":110},"geometry":{"type":"Point","coordinates":[2.8, 49.633]}}
+
+]};
---
+++ params.js
@@ -0,0 +1,1 @@
+var mapQuantClasses = [0, 10, 30, 50, 80, 100]; //size of array should be 6
